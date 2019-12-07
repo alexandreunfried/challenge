2b1f89c352cbe8a75c1f10013258899000caae9d
Alexandre Henrique Unfried <alexandreunfried@gmail.com>
2019-12-06 21:44:55 -0300

    ManufacturerService

--- src/main/java/com/aunfried/challenge/service/ManufacturerService.java
+++ src/main/java/com/aunfried/challenge/service/ManufacturerService.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ManufaturerService {
+public class ManufacturerService {
 
     @Autowired
     private ManufacturerRepository manufacturerRepository;
