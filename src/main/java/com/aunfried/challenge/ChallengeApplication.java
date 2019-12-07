ef71564ab4a2b6df82e3da01e79db099d94bbba4
Alexandre Henrique Unfried <alexandreunfried@gmail.com>
2019-12-06 22:06:50 -0300

    model, repository, service

--- src/main/java/com/aunfried/challenge/ChallengeApplication.java
+++ src/main/java/com/aunfried/challenge/ChallengeApplication.java
@@ -1,4 +1,4 @@
-package com.aunfried.challenge.api;
+package com.aunfried.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
